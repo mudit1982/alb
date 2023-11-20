17c466c286e25dd26e647f31e2764b0d856a4d8b
Mudit Pathak <mudit.pathak@coforge.com>
2023-11-20 12:59:28 +0530

    Update

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -13,14 +13,14 @@ resource "aws_lb_target_group" "front" {
   vpc_id   = var.VPCID
   health_check {
     enabled             = true
-    healthy_threshold   = lookup ( var.target_group , healthy_threshold)
-    interval            = lookup ( var.target_group , interval)
-    matcher             = lookup ( var.target_group , matcher)
-    path                = lookup ( var.target_group , path)
-    port                = lookup ( var.target_group , port)
-    protocol            = lookup ( var.target_group , protocol)
-    timeout             = lookup ( var.target_group , timeout)
-    unhealthy_threshold = lookup ( var.target_group , unhealthy_threshold)
+    healthy_threshold   = lookup ( var.target_group , "healthy_threshold")
+    interval            = lookup ( var.target_group , "interval")
+    matcher             = lookup ( var.target_group , "matcher")
+    path                = lookup ( var.target_group , "path")
+    port                = lookup ( var.target_group , "port")
+    protocol            = lookup ( var.target_group , "protocol")
+    timeout             = lookup ( var.target_group , "timeout")
+    unhealthy_threshold = lookup ( var.target_group , "unhealthy_threshold")
     
   }
 }
